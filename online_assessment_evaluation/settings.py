e66f29c4dde3d9b935f45af790bffdb123681154
Gopal Jadhav <jadhavpatil7620@gmail.com>
2026-06-19 10:16:59 +0530

    Added deployment settings

--- online_assessment_evaluation/settings.py
+++ online_assessment_evaluation/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5hhxglsx#l5tmm%eat!h+07io%r_wjy@$#oc%jo8$ukh937w9e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
