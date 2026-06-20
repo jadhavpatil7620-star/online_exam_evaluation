9567d1fc45c823f0641215637ba2c3489798c63d
Gopal Jadhav <jadhavpatil7620@gmail.com>
2026-06-20 19:53:14 +0530

    Updated allowed hosts

--- online_assessment_evaluation/settings.py
+++ online_assessment_evaluation/settings.py
@@ -29,9 +29,7 @@ SECRET_KEY = 'django-insecure-5hhxglsx#l5tmm%eat!h+07io%r_wjy@$#oc%jo8$ukh937w9e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'oaep-portal.onrender.com',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
